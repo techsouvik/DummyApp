a412594660d23e5bfbc99b25e869a7763686dc59
souvik <souvikaasds01@gmail.com>
2022-04-06 08:29:23 +0530

    page updated

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/i')
+def i():
+    return render_template('i.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
